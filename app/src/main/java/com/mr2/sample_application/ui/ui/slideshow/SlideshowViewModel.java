ae34faa91ae1bad12026320107b71feca5c27d70
Takeru Tajima <mr2o527@gmail.com>
2020-07-08 16:49:54 +0900

    2020/7/7 styles見直し Navigation導入開始

---
+++ app/src/main/java/com/mr2/sample_application/ui/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.mr2.sample_application.ui.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
